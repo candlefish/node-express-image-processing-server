df13434b4b48cfff3a22fc85b3b34af4c720227b
pablopaul <pablopaul@me.com>
2021-06-13 11:34:10 -0700

    Write the filename handler function

--- api/src/router.js
+++ api/src/router.js
@@ -1,4 +1,9 @@
 const { Router } = require('express');
+const { call } = require('ramda');
 router = Router();
+
+function filename(request, file, callback) {
+    callback(null, file.originalname);
+};
 
 module.exports = router;
