b16636c907473238ea12bcd6e41c1c20934b9e62
pablopaul <pablopaul@me.com>
2021-06-13 11:47:04 -0700

    Fix fileFilter callback error type

--- api/src/router.js
+++ api/src/router.js
@@ -10,7 +10,7 @@ function filename(request, file, callback) {
 function fileFilter(request, file, callback) {
     if (file.mimetype !== 'image/png') {
         request.fileValidationError = 'Wrong file type';
-        callback(null, false, {'Wrong file type'});
+        callback(null, false, Error('Wrong file type'));
     } else {
         callback(null, true);
     }
