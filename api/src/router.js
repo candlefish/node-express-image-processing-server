eb88735f4abfe757acfa3746455eba76155845ad
pablopaul <pablopaul@me.com>
2021-06-13 12:06:23 -0700

    Create upload route and responses

--- api/src/router.js
+++ api/src/router.js
@@ -1,9 +1,18 @@
 const { Router } = require('express');
 const multer = require('multer');
+const { router } = require('../app');
 const storage = multer.diskStorage({destination: 'api/uploads/', filename: filename});
 const upload = multer({fileFilter, storage: storage});
 
 router = Router();
+
+router.post('/upload', upload.single('photo'), (request, response) => {
+    if (request.fileValidationError) {
+        return response.status(400).json({error: request.fileValidationError});
+    } else {
+        return response.status(201).json({success: true});
+    }
+});
 
 function filename(request, file, callback) {
     callback(null, file.originalname);
