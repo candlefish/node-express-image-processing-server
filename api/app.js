c348c216434a7b825f3f32f142f4e787cf4129fc
pablopaul <pablopaul@me.com>
2021-06-13 11:05:05 -0700

    Respond with the form

--- api/app.js
+++ api/app.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-
+const pathToIndex = join(__dirname, '../client/index.html');
+app.use('/*', (request, response) => {
+    response.sendFile(pathToIndex);
+});
 module.exports = app;
